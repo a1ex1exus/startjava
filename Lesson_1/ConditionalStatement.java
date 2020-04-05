1b60e7ac3bea8a55e5f81ff1cc050a128ebe943e
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-05 20:54:51 +0200

    implement_HW2_new

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -5,15 +5,11 @@ public class ConditionalStatement {
             System.out.println("You are already an adult.");
         }
 
-        String he = "male";
-        boolean isMail = true;
-        if(isMail) {
+        boolean isMale = true;
+        if(isMale) {
             System.out.println("Hello, brother!");
-        }
-
-        String she = "noMale";
-        boolean isNoMale = true;
-        if(isNoMale) {
+        } 
+        if(!isMale) {
             System.out.println("Women! Take care of man.");
         }
 
@@ -24,10 +20,10 @@ public class ConditionalStatement {
             System.out.println("Your height " + height + " m is above average.");
         }
 
-        char firstLetter = 'I';
-        if(firstLetter == 'M') {
+        char firstNameLetter = 'I';
+        if(firstNameLetter == 'M') {
             System.out.println("Please, open the folder C: \\ M on your computer.");
-        } else if(firstLetter == 'I') {
+        } else if(firstNameLetter == 'I') {
             System.out.println("Please, open the folder C: \\ I on your computer.");
         } else {
             System.out.println("Your name is missing. Please press any key to continue.");
