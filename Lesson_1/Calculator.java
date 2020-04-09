2b7538c33d216982ea666e14b4262350c2b0c694
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-10 00:08:01 +0200

    implement_HW4_new_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,7 +4,7 @@ public class Calculator {
         int b = 5;
         char sign = '^';
         int result;
-        
+
         if(sign == '+') {
             result = a + b;
             System.out.println(a + " + " + b + " = " + result);
@@ -21,7 +21,7 @@ public class Calculator {
         } else if(sign == '^') {
             result = a;
             for(int i = 1; i <= b; i++) {
-                result *= a;
+            result *= a;
             }
             System.out.println(a + " to the " + b + "th degree is equal " + result);
         } else { 
