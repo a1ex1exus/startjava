0e6131a11e11e3b4ad5354ee39c411614a20b853
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-09 16:44:50 +0200

    implement HW_4

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -15,31 +15,31 @@ public class Calculator {
         int divis;
         int mod;
 
-            if(isPlus) {
-                sum = a + b;
-                    System.out.println(a + " + " + b + " = " + sum);
-            }
-                else if(isMinus) {
-                    diff = a - b;
-                        System.out.println(a + " - " + b + " = " + diff);
+        if(isPlus) {
+            sum = a + b;
+            System.out.println(a + " + " + b + " = " + sum);
+        }
+        else if(isMinus) {
+            diff = a - b;
+            System.out.println(a + " - " + b + " = " + diff);
+        }
+        else if(isMultiply) {
+            multip = a * b;
+            System.out.println(a + " * " + b + " = " + multip);
+        }
+        else if(isDivision) {
+            divis = a / b;
+            System.out.println(a + " / " + b + " = " + divis);
+        }
+        else if(isExponention) {
+            int result = a;
+                for(int i = 1; i <= b; i++) {
+                result *= a;
                 }
-                    else if(isMultiply) {
-                        multip = a * b;
-                            System.out.println(a + " * " + b + " = " + multip);
-                    }
-                        else if(isDivision) {
-                            divis = a / b;
-                                System.out.println(a + " / " + b + " = " + divis);
-                        }
-                            else if(isExponention) {
-                                int result = a;
-                                for(int i = 1; i <= b; i++) {
-                                    result *= a;
-                            }
-                                        System.out.println(a + " to the " + b + "th degree is equal " + result);
-                            } else {
-                                     mod = a % b;
-                                            System.out.println("The remainder of dividing " + a + " by " + b + " is " + mod);
-                                }
+        System.out.println(a + " to the " + b + "th degree is equal " + result);
+        } 
+        else { mod = a % b;
+        System.out.println("The remainder of dividing " + a + " by " + b + " is " + mod);
+        }
     }
 }
---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,18 @@
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        byte secretNumber, a;
+             secretNumber = a = 87;
+        byte yourNumber, b;
+             yourNumber = b = 87;
+        if(b>a) {
+            System.out.println("The number you entered is greater than what the computer made");
+        }
+        else if(b<a) {
+            System.out.println("The number you entered is less than what the computer made");
+        }
+        else {
+            System.out.println("You guessed!");
+        }
+    }
+}
