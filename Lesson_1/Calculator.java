e8557e175939e303bfaff22214f55e56f3457cd6
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-09 21:41:19 +0200

    implement_HW4_new2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,7 +4,6 @@ public class Calculator {
         int b = 5;
         char sign = '^';
         int result;
-        float resultDivision;
 
         if(sign == '+') {
             result = a + b;
@@ -16,12 +15,13 @@ public class Calculator {
             result = a * b;
             System.out.println(a + " * " + b + " = " + result);
         } else if(sign == '/') {
+            float resultDivision;
             resultDivision = (float) a / b;
             System.out.printf(a + " / " + b + " = " + "%.2f", resultDivision);
         } else if(sign == '^') {
             result = a;
-         for(int i = 1; i <= b; i++) {
-            result *= a;
+            for(int i = 1; i <= b; i++) {
+                result *= a;
         }
             System.out.println(a + " to the " + b + "th degree is equal " + result);
         } else { 
