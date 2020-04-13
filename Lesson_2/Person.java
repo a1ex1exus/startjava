b4b19577d1789011da725d329c536c8c097462d0
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-13 17:43:57 +0200

    implement_HW1_new

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,4 +1,5 @@
 public class Person {
+
     String sex = "Male";
     String name = "Arthur";
     String nativeLanguage = "French";
@@ -11,18 +12,23 @@ public class Person {
     void walk() {
         System.out.println(name + " goes at a speed of " + walkingSpeed + " km per hour");
     }
+
     void run() {
         System.out.println(name + " runs at speed " + runningSpeed + " km per hour");
     }
+
     void speak() {
         System.out.println(name + " speaks " + nativeLanguage);
     }
+
     void learnJava() {
     }
+
     boolean wake() {
         System.out.println(name + "is sleeping");
         return false;
     }
+
     String read() {
         return "nothing";
     }
