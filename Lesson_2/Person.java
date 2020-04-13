a09c3206a72ba015f77d8d929906b63552349644
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-13 13:23:57 +0200

    implement_HW1

---
+++ Lesson_2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    String sex = "Male";
+    String name = "Arthur";
+    String nativeLanguage = "French";
+    float height = 1.81f;
+    int weight = 75;
+    int walkingSpeed = 6;
+    int runningSpeed = 10;
+    int age = 50;
+
+    void walk() {
+        System.out.println(name + " goes at a speed of " + walkingSpeed + " km per hour");
+    }
+    void run() {
+        System.out.println(name + " runs at speed " + runningSpeed + " km per hour");
+    }
+    void speak() {
+        System.out.println(name + " speaks " + nativeLanguage);
+    }
+    void learnJava() {
+    }
+    boolean wake() {
+        System.out.println(name + "is sleeping");
+        return false;
+    }
+    String read() {
+        return "nothing";
+    }
+}
+
