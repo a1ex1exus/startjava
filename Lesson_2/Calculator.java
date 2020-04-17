c2f1e4f2be879f77cb73e57ec4897a87e0963869
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-17 14:18:13 +0200

    implement_HW4_new2

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -8,86 +8,84 @@ public class Calculator {
     private String mathSign = keyboard.next();
     private int result;
 
-// sledujuschie 3 stroki otnosjatsa k 26,27 strokam CalculatorTest.java
+// Sledujuschie 3 stroki otnosjatsa k 26,27 strokam CalculatorTest.java
     private String choice = keyboard.next();
     public String getChoice() {
     return choice;
     }
 
     public int getNumber1() {
-        return number1;
+    return number1;
     }
     public int getNumber2() {
     return number2;
     }
     public String getMathSign() {
-        return mathSign;
+    return mathSign;
     }
-//     BLock s metodami:
+//BLock s metodami:
        while(true) {
-         switch (mathSign) {
+            switch (mathSign) {
 
-//           public void addition() {}
-            case "+":
-            int result;
-            result = number1 + number2;
-            System.out.println(number1 + " + " + number2 + " = " + result);
-                break; 
+//public void addition() {}
+                case "+":
+                int result;
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                    break; 
 
-//            public void substruction() {}
-            case "-":
-            result = number1 - number2;
-            System.out.println(number1 + " - " + number2 + " = " + result);
-                break;
+//public void substruction() {}
+                case "-":
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                    break;
 
-//            public void multyplyng() {}
-            case "*":
-            result = number1 * number2;
-            System.out.println(number1 + " * " + number2 + " = " + result);
-                break;
+//public void multyplyng() {}
+                case "*":
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                    break;
 
-//            public void division() {}
-            case "/":
-            result = number1 / number2;
-            System.out.println(number1 + " / " + number2 + " = " + result);
-                break;
+//public void division() {}
+                case "/":
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                    break;
 
-//            public void exponentiation() {}
-            case "^":
+//public void exponentiation() {}
+                case "^":
                 result = number1;
-                for(int i = 1; i < number2; i++) {
-                result *= number1;
-                }
-            System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
-                break;
+                    for(int i = 1; i < number2; i++) {
+                    result *= number1;
+                    }
+                System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
+                    break;
 
-//            public void modulo() {}
-            case "%":
-            result = number1 % number2;
-            System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
-                break;
+//public void modulo() {}
+                case "%":
+                result = number1 % number2;
+                System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
+                    break;
 
-//            public void errorAnotherMathSign() {}
-            default:
+//public void errorAnotherMathSign() {}
+                default:
             System.out.println("Error, enter another arithmetic sign");
 
-//           Running the script to continue or finish
+//Running the script to continue or finish
 
-
-
-//           public void inputMistake() {}
+//public void inputMistake() {}
                 while(!choice.equalsIgnoreCase("n") && !choice.equalsIgnoreCase("y")) {
                 System.out.println("You are mistaken. Press y or n");
                 choice = keyboard.next();
                 }
-//           public void exit() {}
+//public void exit() {}
                 if(choice.equalsIgnoreCase("n")) {
                     System.out.println("Goodbye! Until next time!");
                     break;
-//           public void toContinue() {}
+//public void toContinue() {}
                 } 
                 if(choice.equalsIgnoreCase("y")) {
-                continue;
+                    continue;
                 }
         }
     }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,31 +4,27 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator CalculatorOne = new Calculator();
 
+        System.out.println("Enter any integer: ");
+        System.out.println(CalculatorOne.getNumber1());
 
-            System.out.println("Enter any integer: ");
-            System.out.println(CalculatorOne.getNumber1());
+        System.out.println("Enter the sign of the action you want to perform:+,-,*,/,^,%");
+        System.out.println(CalculatorOne.getMathSign());
 
-            System.out.println("Enter the sign of the action you want to perform:+,-,*,/,^,%");
-            System.out.println(CalculatorOne.getMathSign());
+        System.out.println("Enter another integer: ");
+        System.out.println(CalculatorOne.getNumber2());
 
-            System.out.println("Enter another integer: ");
-            System.out.println(CalculatorOne.getNumber2());
+        calculatorOne.addition();
+        calculatorOne.substruction();
+        calculatorOne.multyplyng();
+        calculatorOne.division();
+        calculatorOne.exponentiation();
+        calculatorOne.modulo();
 
+        System.out.println("Do you want to continue? Press y or n");
+        System.out.println(CalculatorOne.getChoice());
 
-calculatorOne.addition();
-calculatorOne.substruction();
-calculatorOne.multyplyng();
-calculatorOne.division();
-calculatorOne.exponentiation();
-calculatorOne.modulo();
-
-
-            System.out.println("Do you want to continue? Press y or n");
-            System.out.println(CalculatorOne.getChoice());
-
-calculatorOne.imputMistake();
-calculatorOne.exit();
-calculatorOne.toContinue();
-
+        calculatorOne.imputMistake();
+        calculatorOne.exit();
+        calculatorOne.toContinue();
     }
 }
