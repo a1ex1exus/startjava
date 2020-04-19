29460aaa3ffef6fb69b82904ddc9d0353d91c5b6
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-19 16:34:33 +0200

    implement_HW5_new4

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,46 +1,41 @@
 public class Calculator {
-    int number1;
-    int number2;
-    String mathSigh;
-    int result;
+    private int number1;
+    private int number2;
+    private char mathSigh;
+    private int result;
 
-    public String calculate() {
+    public int calculate() {
         switch(mathSign) {
-            case "+":
-            result = number1 + number2;
-            System.out.println(number1 + " + " + number2 + " = " + result);
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
                 break; 
-
-            case "-":
-            result = number1 - number2;
-            System.out.println(number1 + " - " + number2 + " = " + result);
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
-
-            case "*":
-            result = number1 * number2;
-            System.out.println(number1 + " * " + number2 + " = " + result);
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
-
-            case "/":
-            result = number1 / number2;
-            System.out.println(number1 + " / " + number2 + " = " + result);
+            case '/':
+                float resultDivision;
+                resultDevision = (float)number1 / number2;
+                System.out.printf(number1 + " / " + number2 + " = " + "%.2f\n", resultDivision);
                 break;
-
-            case "^":   
-            result = number1;
+            case '^':   
+                result = number1;
                 for(int i = 1; i < number2; i++) {
                 result *= number1;
                 }
-            System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
+                System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
                 break;
-
-            case "%":
-            result = number1 % number2;
-            System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
+            case '%':
+                result = number1 % number2;
+                System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
                 break;
-
             default:
-            System.out.println("Error, enter another arithmetic sign");
+                System.out.println("Error, enter another arithmetic sign");
         }
     }
 }
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculator1 = new Calculator();
+        Calculator calculator = new Calculator();
         Scanner keyboard = new Scanner(System.in);
         System.out.println(" Enter any integer: ");
-        calculator1.number1 = keyboard.nextInt();
+        calculator.number1 = keyboard.nextLine().charAt(0);
         System.out.println("Enter the sign of the action you want to perform:+,-,*,/,^,%");
-        calculator1.mathSign = keyboard.nextLine();
+        calculator.mathSign = keyboard.next();
         System.out.println("Enter another integer: ");
-        calculator1.number2 = keyboard.nextInt();
+        calculator.number2 = keyboard.nextInt();
+        char choice = keyboard.nextLine().charAt(0);
         do {
-            calculator1.calculate();
-            System.out.println("Do you want to continue? Press y or n");
-            String choice = keyboard.next();
-            if(!choice.equalsIgnoreCase("n") && !choice.equalsIgnoreCase("y")) {
+            calculator.calculate();
+        } while (
+            (System.out.println("Do you want to continue? Press y or n");
+            if(!choice.equalsIgnoreCase('n') && !choice.equalsIgnoreCase('y')) {
                 System.out.println("You are mistaken. Press y or n");
-                choice = keyboard.next();
                 }
-            if(choice.equalsIgnoreCase ("n")) {
+            if(choice.equalsIgnoreCase ('n')) {
                 System.out.println("Goodbye! Until next time!");
                 break;
-            } else {}
-        } while (true); 
+            }
+            )
     }
 }
