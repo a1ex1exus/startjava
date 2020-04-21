20344d1c7a99e3d881b75e1fe9cb8ba74f6c1ea7
Oleg Makarov <oleh.makarov@gmx.de>
2020-04-21 02:33:10 +0200

    implement_HW5_new7

---
+++ Lesson_2/Calculator/Calculator.java
@@ -0,0 +1,52 @@
+public class Calculator {
+    private int number1;
+    private int number2;
+    private char mathSign;
+    private int result;
+
+    public void setNumber1(int number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+  
+    public void  setMathSign(char mathSign) {
+        this.mathSign = mathSign;
+    }
+
+     public void calculate() {
+        switch(mathSign) {
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break; 
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+            case '/':
+                float resultDevision = (float)number1 / number2;
+                System.out.printf(number1 + " / " + number2 + " = " + "%.2f\n", resultDevision);
+                break;
+            case '^':   
+                result = number1;
+                for(int i = 1; i < number2; i++) {
+                result *= number1;
+                }
+                System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
+                break;
+            case '%':
+                result = number1 % number2;
+                System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
+                break;
+            default:
+                System.out.println("Error, enter another arithmetic sign");
+        }
+    }
+}
---
+++ Lesson_2/Calculator/CalculatorTest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        while(true) {
+            Calculator calculator = new Calculator();
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("Enter any integer: ");
+            calculator.setNumber1(keyboard.nextInt());
+            System.out.println("Enter the sign of the action you want to perform:+,-,*,/,^,%.");
+            calculator.setMathSign(keyboard.next().charAt(0));
+            System.out.println("Enter any another integer: ");
+            calculator.setNumber2(keyboard.nextInt());
+           
+            calculator.calculate();
+
+            System.out.println("Do you want to continue? Press y or n");
+            String choice = keyboard.next();
+                while(!choice.equalsIgnoreCase("n") && !choice.equalsIgnoreCase("y")) {
+                System.out.println("You are mistaken. Press y or n");
+                choice = keyboard.next();
+                    } if(choice.equalsIgnoreCase("n")) {
+                    System.out.println("Goodbye! Until next time!");
+                    break;
+                    } if(choice.equalsIgnoreCase("y")) {
+                    continue;
+                    }
+        }
+    }
+}
