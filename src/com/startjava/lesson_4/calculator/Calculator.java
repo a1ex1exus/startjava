2df60427fceb4d157d85ab05dd7372be550dafba
Oleg Makarov <oleh.makarov@gmx.de>
2020-05-11 00:34:25 +0200

    HomeWork1_Update1

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -5,34 +5,66 @@ public class Calculator {
     private int number2;
     private String mathSign;
 
+    public Calculator(int number1, String mathSign, int number2) {
+        this.number1 = number1;
+        this.mathSign = mathSign;
+        this.number2 = number2;
+    }
+
     public void setNumber1(int number1) {
         this.number1 = number1;
     }
 
-    public void setNumber2(int number2) {
-        this.number2 = number2;
+    public int getNumber1() {
+        return number1;
     }
 
     public void setMathSign(String mathSign) {
         this.mathSign = mathSign;
     }
 
+    public String getMathSign() {
+        return mathSign;
+    }
+
+    public void setNumber2(int number2) {
+        this.number2 = number2;
+    }
+
+    public int getNumber2() {
+        return number2;
+    }
+
     public void calculate() {
-        switch (mathSign) {
-            case "abs":
-                System.out.println("Absolute value of the number " + number1 + " is " + Math.abs(number1));
+        switch(mathSign) {
+            case "+":
+                int result;
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
-            case "sqrt":
-                System.out.println("Square root of " + number1 + " is " + Math.sqrt(number1));
+            case "-":
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
-            case "pow":
-                System.out.println(number1 + " to the " + number2 + "th degree is equal " + Math.pow(number1, number2));
+            case "*":
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
-            case "cbrt":
-                System.out.println("Cube root from " + number1 + " is " + Math.cbrt(number1));
+            case "/":
+                float resultDiv = (float) number1 / number2;
+                System.out.printf(number1 + " / " + number2 + " = " + "%.2f\n", resultDiv);
+                break;
+            case "^":
+                result = (int) Math.pow(number1, number2);
+                System.out.println(number1 + " to the " + number2 + "th degree is equal " + result);
+                break;
+            case "%":
+                result = (int) Math.IEEEremainder(number1, number2);
+                System.out.println("The remainder of dividing " + number1 + " by " + number2 + " is " + result);
                 break;
             default:
-                System.out.println("Error, enter another arithmetic operation");
+                System.out.println("Error, enter another arithmetic sign");
         }
     }
 }
+
--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -6,30 +6,28 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         String choice = "y";
-        while (choice.equalsIgnoreCase("y")) {
-            Calculator calculator = new Calculator();
-            System.out.print("Enter separated by a space ");
-            System.out.print("any integer, math operation, \nif necessary another integer or zero: ");
-            String[] mathOperation = new String[5];
-            mathOperation[0] = keyboard.nextLine();
-            String a = mathOperation[0];
+        while(choice.equalsIgnoreCase("y")) {
+            System.out.print("Enter math expression of type 'a + b': ");
+            String string = keyboard.nextLine();
 
-            String[] parts = a.split(" ");
-            String part1 = parts[0];
-            String part2 = parts[1];
-            String part3 = parts[2];
+            String[] mathExpression = string.split(" ");
+            String number1 = mathExpression[0];
+            String mathSign = mathExpression[1];
+            String number2 = mathExpression[2];
 
-            calculator.setNumber1(Integer.parseInt(part1));
-            calculator.setMathSign(part2);
-            calculator.setNumber2(Integer.parseInt(part3));
+            Calculator calculator = new Calculator(
+                    Integer.parseInt(number1),
+                    mathSign,
+                    Integer.parseInt(number2)
+            );
 
             calculator.calculate();
 
             System.out.println("Do you want to continue? Press y or n");
-            choice = keyboard.next();
-            while (!choice.equalsIgnoreCase("n") && !choice.equalsIgnoreCase("y")) {
+            choice = keyboard.nextLine();
+            while(!choice.equalsIgnoreCase("n") && !choice.equalsIgnoreCase("y")) {
                 System.out.println("You are mistaken. Press y or n");
-                choice = keyboard.next();
+                choice = keyboard.nextLine();
             }
         }
         System.out.println("Goodbye! Until next time!");
