4f08ac1d97e9651c6202fddbf1b0c39b5f08da41
Oleg Makarov <oleh.makarov@gmx.de>
2020-05-11 03:03:01 +0200

    HomeWork2_Update2

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -1,5 +1,18 @@
+
+//TODO:     GuessNumber
+//TODO:      player1.addNumberToArray(i, number); можно поместить в enterNumber
+//TODO:
+// все методы, которые не вызываются за пределами класса сделай private
+// в printNumbers(int[] numbers) можно передать игрока целиком и использовать
+// там же его имя, а не в finishGame
+// метод называется printNumbers, но он ничего не печатает.
+// В нем же и выводи числа. Сохранять их предварительно в строку не нужно.
+
+
+
 package lesson_4.game;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,42 +35,56 @@ public class GuessNumber {
         System.out.println("Ok, let's play! You have 10 attempts.");
 
         for (int i = 0; i < player1.getNumbers().length; i++) {
-            String player1Name = player1.getName();
-            int number = enterNumber(keyboard, player1Name);
-            player1.addNumberToArray(i, number);
-            if (player1.checkNumber(number, randomNumber, i)) {
+
+            player1.enterNumber(player1.getName(), keyboard, i, int number, numbers);
+
+            if (player1.checkNumber(player1.getName(), number, randomNumber, i))) {
                guessed = true;
                 break;
             }
 
-            number = enterNumber(keyboard, player2.getName());
-            player2.addNumberToArray(i, number);
-            if (player2.checkNumber(number, randomNumber, i)) {
+            player2.enterNumber(player2.getName(), keyboard, i, number, numbers);
+
+            if (player2.checkNumber(player2.getName(), number, randomNumber, i))) {
                 guessed = true;
                 break;
             }
         }
         if (!guessed) {
-            finishGame(randomNumber);
+            printNumbers(randomNumber);
         }
     }
-    private int enterNumber(Scanner keyboard, String name) {
+
+     private int enterNumber(String name, Scanner keyboard, int i, int number, int[] numbers) {
         System.out.println(name + ", enter any integer from 1 to 100.");
-        return keyboard.nextInt();
+        number = keyboard.nextInt();
+        if ((number > 100) && (number < 1)) {
+            System.out.println("The number you entered is less than 1 or greater than 100. Repeat the enter.");
+        } else {
+            numbers[i] = number;
+        }
+        return numbers[i];
     }
 
-    void finishGame(int randomNumber) {
+    private boolean checkNumber(String name, int randomNumber, int number,  int i) {
+        if (randomNumber < number) {
+            System.out.println(name + ", you entered number " + number + ". It is greater than the computer made up.");
+            System.out.println("And now another player is trying to guess.");
+            return false;
+        } else if (randomNumber > number) {
+            System.out.println(name + ", you entered number " + number + ". It is less than the computer made up.");
+            System.out.println("Now another player is trying to guess.");
+            return false;
+        } else {
+            System.out.println(name + ", you guessed number " + number + " from attempt number " + i + ". \nYou won!");
+            return true;
+        }
+    }
+
+    void printNumbers(int randomNumber) {
         System.out.println(player1.getName() + " ran out of attempts. Game over.");
         System.out.println("Computer made up a number: " + randomNumber);
-        System.out.println(player1.getName() + " entered the numbers: " + printNumbers(player1.getNumbers()));
-        System.out.println(player2.getName() + " entered the numbers: " + printNumbers(player2.getNumbers()));
-    }
-
-    String printNumbers(int[] numbers) {
-        String outputArray = "";
-        for (int number : numbers) {
-            outputArray += number + " ";
-        }
-        return outputArray;
+        System.out.println(player1.getName() + " entered the numbers: " + Arrays.toString(player1.getNumbers()));
+        System.out.println(player2.getName() + " entered the numbers: " + Arrays.toString(player2.getNumbers()));
     }
 }
--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -1,8 +1,14 @@
+
+//TODO:
+// addNumberToArray setNumber
+// checkNumber верни в класс игры
+// в checkNumber вместо трех return можно сделать два
+
 package lesson_4.game;
 
 public class Player {
     private String name;
-    private int[] numbers = new int[10];
+    public int[] numbers = new int[10];
 
     public Player(String name) {
         this.name = name;
@@ -16,26 +22,4 @@ public class Player {
         return numbers;
     }
 
-    public void addNumberToArray(int i, int number) {
-        if ((number > 100) && (number < 1)) {
-            System.out.println("The number you entered is less than 1 or greater than 100. Repeat the enter.");
-        } else {
-            numbers[i] = number;
-        }
-    }
-
-    public boolean checkNumber(int number, int randomNumber, int i) {
-        if (randomNumber < number) {
-            System.out.println(name + ", you entered number " + number + ". It is greater than the computer made up.");
-            System.out.println("And now another player is trying to guess.");
-            return false;
-        } else if (randomNumber > number) {
-            System.out.println(name + ", you entered number " + number + ". It is less than the computer made up.");
-            System.out.println("Now another player is trying to guess.");
-            return false;
-        } else {
-            System.out.println(name + ", you guessed number " + number + " from attempt number " + i + ". \nYou won!");
-            return true;
-        }
-    }
 }
